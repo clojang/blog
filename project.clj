16f065ef25fe40c69096f109a0bd8c41749d6a0f
Duncan McGreggor <oubiwann@gmail.com>
2017-05-05 14:30:44 -0500

    Updated deps.

--- project.clj
+++ project.clj
@@ -54,5 +54,5 @@
         [lein-ancient "0.6.10"]
         [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
         [lein-bikeshed "0.4.1" :exclusions [org.clojure/tools.namespace]]
-        [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
+        [lein-kibit "0.1.4" :exclusions [org.clojure/clojure]]
         [venantius/yagni "0.1.4"]]}})
